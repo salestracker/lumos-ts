87c9e62cf3e48eb887b073e7a32e647251d5f876
codecakes <codecakes@users.noreply.github.com>
2025-03-30 14:49:25 +0530

    Fix/add dist (#5)
    * fix: polyfill process variable for browser build
    
    * the lumos-ts package's package.json correctly point to its declaration file to resolve the TypeScript error?
    
    * Fixed path - declaration files are in dist/ not dist/types/
    
    * TypeScript will generate:JavaScript files at: dist/index.jsDeclaration files at: dist/index.d.tsBut your package.json was looking for them at dist/types/index.d.ts which doesn't match the compiler output location.

---
+++ dist/repositories/baseRepository.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BaseRepository = void 0;
+class BaseRepository {
+    constructor(db) {
+        this.db = db;
+    }
+    // Execute a SQL query with optional parameters.
+    async execute(query, params = []) {
+        return this.db.exec(query, params);
+    }
+}
+exports.BaseRepository = BaseRepository;
+//# sourceMappingURL=baseRepository.js.map
