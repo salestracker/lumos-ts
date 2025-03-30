02ba5d5474e18eae316af8d5d2c6692f17d8f554
codecakes <codecakes@users.noreply.github.com>
2025-03-30 14:25:31 +0530

    the lumos-ts package's package.json correctly point to its declaration file to resolve the TypeScript error? (#3)
    * fix: polyfill process variable for browser build
    
    * the lumos-ts package's package.json correctly point to its declaration file to resolve the TypeScript error?

---
+++ dist/core/trpc.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.procedure = exports.router = void 0;
+const server_1 = require("@trpc/server");
+const t = server_1.initTRPC.create();
+exports.router = t.router;
+exports.procedure = t.procedure;
---
+++ dist/modules/health.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.healthRouter = void 0;
+const trpc_1 = require("../core/trpc");
+exports.healthRouter = (0, trpc_1.router)({
+    check: trpc_1.procedure.query(() => {
+        return { status: 'ok' };
+    }),
+});
